c867fd42861f65ede546b25ed0d3e009d696e43c
Ujjawal Gupta <ujjawalg047@gmail.com>
2026-09-04 19:09:51 +0530

    Fix: git case sensitive issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ import LandingPage from "../components/LandingPage";
 import { auth } from "../auth";
 import { prisma } from "../lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   let session: any = null;
   let testimonial: any[] = [];
